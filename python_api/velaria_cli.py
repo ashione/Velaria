3f2ae93726bdc5e86f231994678389298f496afd
zuolingxuan <zuolingxuan@bytedance.com>
2026-04-02 22:50:27 +0800

    python: preserve velaria_cli module compatibility

--- python_api/velaria_cli.py
+++ python_api/velaria_cli.py
@@ -1,5 +1,10 @@
-from velaria.cli import *  # noqa: F403
+import sys
+
+import velaria.cli as _velaria_cli_impl
+
+
+sys.modules[__name__] = _velaria_cli_impl
 
 
 if __name__ == "__main__":
-    raise SystemExit(main())
+    raise SystemExit(_velaria_cli_impl.main())
